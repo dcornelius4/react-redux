30d04fd6c7ab90e0a6563c2d041fbf6744600fa2
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-22 15:01:05 -0700

    welp I made a lunch box

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  drink: null,
+  sandwich: null,
+  chips: null
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+    case 'ADD_SANDWICH':
+      return { ...state, sandwich: action.payload };
+    case 'ADD_CHIPS':
+      return { ...state, chips: action.payload };
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
+    case 'REMOVE_SANDWICH':
+      return { ...state, sandwich: null };
+    case 'REMOVE_CHIPS':
+      return { ...state, chips: null };
+    case 'REMOVE_LUNCH':
+      return { initialState };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'Jarito'
+});
+
+console.log('added drink', store.getState());
+
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'Turkey and Cheese'
+});
+
+console.log('added sandwich', store.getState());
+
+store.dispatch({
+  type: 'ADD_CHIPS',
+  payload: 'Sea Salt and Vinegar'
+});
+
+console.log('added chips', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_DRINK',
+  payload: 'Jarito'
+});
+
+console.log('removed drink', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_SANDWICH',
+  payload: 'Turkey and Cheese'
+});
+
+console.log('removed sandwich', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_CHIPS',
+  payload: 'Sea Salt and Vinegar'
+});
+
+console.log('removed chips', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_LUNCH',
+});
+
+console.log('no mo lunch', store.getState());
