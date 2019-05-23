c3f08455f1eeeac489d7147250a8de49e5c99b5a
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-23 16:54:29 -0700

    created appcomponent/got it to render with indexjs

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AllPosts from '../containers/posts/AllPosts';
+import CreatePost from '../containers/posts/CreatePost';
+
+export default function App() {
+  return (
+    <>
+      <CreatePost />
+      <AllPosts title="Wassup" />
+    </>
+  );
+}
