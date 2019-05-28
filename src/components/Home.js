3566076f9e5a60c97e3ff9fb37752958297f0dc3
dcornelius4 <d_cornelius94@yahoo.com>
2019-05-28 09:41:42 -0700

    changed app js to home js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,17 @@ import React from 'react';
 import AllPosts from '../containers/posts/AllPosts';
 import CreatePost from '../containers/posts/CreatePost';
 
-export default function App() {
+export default function Home() {
   return (
     <>
+    <h1>Posts</h1>
+    <section>
+      <h2>Create Post</h2>
       <CreatePost />
-      <AllPosts title="Wassup" />
+    </section>
+    <section>
+      <AllPosts />
+    </section>
     </>
   );
 }
